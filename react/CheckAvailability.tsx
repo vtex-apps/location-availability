19e8543fd37f7f602e730dc9622203ae05159ccb
Wender Lima <wender.lima@vtex.com.br>
2020-09-02 18:55:59 -0300

    Fixing lint warning; fixing Storefront definition file

--- react/CheckAvailability.tsx
+++ react/CheckAvailability.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import React from 'react'
 import {
--- react/typings/storefront.d.ts
+++ react/typings/storefront.d.ts
@@ -10,3 +10,4 @@ declare global {
     getSchema?(props: P): object
     schema: object
   }
+}
